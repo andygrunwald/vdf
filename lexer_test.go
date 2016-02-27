19a741e41149a9a69d7be62102df907d5e82f0a6
Andy Grunwald <andygrunwald@gmail.com>
2016-02-27 23:31:04 +0100

    Added first unit tests for lexer / scanner

---
+++ lexer_test.go
@@ -0,0 +1,44 @@
+package vdf_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/andygrunwald/vdf"
+)
+
+// Ensure the scanner can scan tokens correctly.
+func TestScanner_Scan(t *testing.T) {
+	var tests = []struct {
+		s   string
+		tok vdf.Token
+		lit string
+	}{
+		// Special tokens (EOF, ILLEGAL, WS)
+		{s: ``, tok: vdf.EOF},
+		{s: `#`, tok: vdf.ILLEGAL, lit: `#`},
+		{s: ` `, tok: vdf.WS, lit: " "},
+		{s: "\t", tok: vdf.WS, lit: "\t"},
+		{s: "\n", tok: vdf.WS, lit: "\n"},
+
+		// Misc characters
+		{s: `{`, tok: vdf.CURLY_BRACE_OPEN, lit: "{"},
+		{s: `}`, tok: vdf.CURLY_BRACE_CLOSE, lit: "}"},
+		{s: `"`, tok: vdf.QUOTATION_MARK, lit: "\""},
+		{s: `\`, tok: vdf.ESCAPE_SEQUENCE, lit: "\\"},
+
+		// Identifiers
+		{s: `foo`, tok: vdf.IDENT, lit: `foo`},
+		{s: `Zx12_3U_-`, tok: vdf.IDENT, lit: `Zx12_3U_`},
+	}
+
+	for i, tt := range tests {
+		s := vdf.NewScanner(strings.NewReader(tt.s))
+		tok, lit := s.Scan(false)
+		if tt.tok != tok {
+			t.Errorf("%d. %q token mismatch: exp=%q got=%q <%q>", i, tt.s, tt.tok, tok, lit)
+		} else if tt.lit != lit {
+			t.Errorf("%d. %q literal mismatch: exp=%q got=%q", i, tt.s, tt.lit, lit)
+		}
+	}
+}
